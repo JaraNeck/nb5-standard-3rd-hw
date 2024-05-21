b1c35d75fe937eb3cac191891772f4a20e9146dc
unknown <choipoe@gmail.com>
2024-05-21 10:41:09 +0900

    완료

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ export default function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((student) => student.age >= 18);
 
+  // In HTML: <element onclick="myScript">
+  // In JavaScript: object.onclick = function(){myScript};
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        {filteredStudents.map((student) => (
+          <li
+            key={student.name}
+            onClick={() => alert(`${student.age}, ${student.grade}`)} // alert는 문자열만 받기 때문에 ${}과 `을 써야한다.
+          >
+            {student.name}
+          </li>
+        ))}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
